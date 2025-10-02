820abeb6245ed55f21a9f27895b52fce33fc784d
YanaKachalova <yc-user@epdi71s3mkfoafgpblv4.auto.internal>
2025-10-02 11:26:33 +0000

    Четвёртая версия.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 if not SECRET_KEY:
